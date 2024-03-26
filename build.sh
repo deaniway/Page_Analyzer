de84e617f64fcdacc18c5393e209d50aaa8212b4
Denis <ad.way@mail.ri>
2024-03-26 18:27:14 +0400

    fix sh2

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-export DATABASE_URL=postgresql://pguser:pgpass@localhost:5433/pgdb
-make install && psql -a -d $DATABASE_URL -f database.sql
+docker exec -i dev_page_analyzer psql -U pguser -d pgdb -f database.sql
 
 
