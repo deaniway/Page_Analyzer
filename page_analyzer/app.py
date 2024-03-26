f8639d03a2f183e3c56fa4e953b060539f36dfac
Denis <ad.way@mail.ri>
2024-03-26 20:07:44 +0400

    fix app5

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -41,7 +41,7 @@ def urls():
         return redirect(url_for('get_url_list', id=url_id))
     url = manager.insert_url(normal_url)
     flash(f'Страница успешно добавлена', 'success')
-    return redirect(url_for('get_url_list', id=url.id))
+    return redirect(url_for('get_url_list', id=url.id)), 200
 
 
 @app.get('/urls')
